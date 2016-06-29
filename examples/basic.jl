9e70e3a56f68857401061430380f074367d1e230
femtotrader <femto.trader@gmail.com>
2016-06-29 09:00:34 +0200

    Fixed

--- examples/basic.jl
+++ examples/basic.jl
@@ -1,14 +1,41 @@
 using PubSub: pub, subscribe, sendMessage
 
-function listener1(arg1, arg2=nothing)
+function listener0()
+    println("Function listener0")
+end
+
+function listener1(arg1)
     println("Function listener1 received:")
-    println("   arg1 =$arg1")
-    println("   arg2 =$arg2")
+    println("   arg1: $arg1")
+end
+
+function listener2(args...)
+    println("Function listener1 received:")
+    println("   args: $args")
+    println("multiple positional args")
+end
+
+function listener3(arg1; a=-1, b="")
+    println("Function listener1 received:")
+    println("   arg1: $arg1")
+    println("   a: $a")
+    println("   b: $b")
+end
+
+function listener4(args...; kwargs...)
+    println("Function listener1 received:")
+    println("   args: $args")
+    println("   kwargs: $kwargs")
 end
 
 # ------------ register listener ------------------
 
-subscribe(pub, listener1, "rootTopic")
+println("Subscribe listeners to topic")
+#subscribe(pub, listener0, "rootTopic")
+#subscribe(pub, listener1, "rootTopic")
+#subscribe(pub, listener2, "rootTopic")
+subscribe(pub, listener3, "rootTopic")
+#subscribe(pub, listener4, "rootTopic")
 
 #---------------- send a message ------------------
 
@@ -18,4 +45,7 @@ anObj = Dict(
     "b" => "abc"
 )
 
-sendMessage(pub, "rootTopic") #; arg1=123, arg2=anObj)
+#sendMessage(pub, "rootTopic", 123, 456)
+
+#sendMessage(pub, "rootTopic", 123; arg2=anObj)
+sendMessage(pub, "rootTopic", 123; a=anObj["a"], b=anObj["b"])
--- src/PubSub.jl
+++ src/PubSub.jl
@@ -1,6 +1,6 @@
 module PubSub
 
-    using DataStructures: OrderedDict, Queue
+    using DataStructures: OrderedDict, OrderedSet
 
     type MainPubSub
     end
@@ -13,27 +13,47 @@ module PubSub
         s::AbstractString
     end
 
+    function Base.hash(topic::Topic)
+        hash(topic.s)
+    end
+
+    function Base.isequal(topic1::Topic, topic2::Topic)
+        topic1.s == topic2.s
+    end
+
+
     type Proxy
-        d::OrderedDict{Topic,Queue}
+        #d_listener::OrderedDict{AbstractString,Listener}
+        #d_listener::OrderedDict{Topic,Listener}
+        d_listener::OrderedDict{Topic,OrderedSet{Listener}}
+        #d_queue::OrderedDict{Topic,Listener}
         Proxy() = new(OrderedDict())
     end
 
-    proxy= Proxy()
+    proxy = Proxy()
 
     function subscribe(pub::MainPubSub, listener::Listener, topic::AbstractString)
         subscribe(pub::MainPubSub, listener::Listener, Topic(topic))
     end
 
     function subscribe(pub::MainPubSub, listener::Listener, topic::Topic)
-        error("ToDo")
+        if !(haskey(proxy.d_listener, topic))
+            proxy.d_listener[topic] = OrderedSet{Listener}()
+        end
+        push!(proxy.d_listener[topic], listener)
     end
 
-    function sendMessage(pub::MainPubSub, topic::AbstractString; kwargs...)
-        sendMessage(pub, Topic(topic); kwargs...)
+    function sendMessage(pub::MainPubSub, topic::AbstractString, args...; kwargs...)
+        sendMessage(pub, Topic(topic), args...; kwargs...)
     end
 
-    function sendMessage(pub::MainPubSub, topic::Topic; kwargs...)
-        error("ToDo")
+    function sendMessage(pub::MainPubSub, topic::Topic, args...; kwargs...)
+        if haskey(proxy.d_listener, topic)
+            for listener in proxy.d_listener[topic]
+                println("$listener $args $kwargs")
+                listener(args...; kwargs...)
+            end
+        end
     end
 
 end # module
